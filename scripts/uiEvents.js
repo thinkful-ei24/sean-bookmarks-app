dd23885b4ae355e08d993fcc73212651d82812b7
Sean Phelan <seanphelan97@gmail.com>
2018-08-30 08:39:31 -0600

    add new form features

--- scripts/uiEvents.js
+++ scripts/uiEvents.js
@@ -11,9 +11,19 @@ const uiEventHandlers = (function() {
     handleMinRatingChange();
   }
 
+  $.fn.extend({
+    serializeJson: function() {
+      const formData = new FormData(this[0]);
+      const singleObject = {};
+      formData.forEach((val, name) => singleObject[name] = val);
+      return JSON.stringify(singleObject);
+    }
+  });
+
   function handleBookmarkAdd() {
     $('#bookmark-app-form').submit(event => {
       event.preventDefault();
+      console.log($(event.target).serializeJson());
       console.log('add bookmark');
     });
   }
